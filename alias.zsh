5614acecfdf788207cca79233cc16174e6051f18
Predrag Mijatovic <developej@gmail.com>
2013-08-06 22:52:17 +0200

    alias stuff

---
+++ alias.zsh
@@ -0,0 +1,35 @@
+# aliases
+# push and pop directories on directory stack
+alias pu="pushd"
+alias po="popd"
+
+# basic directory operations
+alias ...="cd ../.."
+alias -- -="cd -"
+
+# show history
+alias history="fc -l 1"
+
+# list all
+alias l="ls -alh"
+# count all
+alias countall="ls | wc -l"
+# list files
+alias lf="ls -alh | grep ^-"
+# count files
+alias countfiles="ls -l | grep ^- | wc -l"
+# list directories
+alias ld="ls -alh | grep ^d"
+# count directories
+alias countdirs="ls -l | grep ^d | wc -l"
+# list symbolic links
+alias ll="ls -alh | grep ^l"
+# count symbolic links
+alias countsymlinks="ls -l | grep ^l | wc -l"
+
+# pacman aliases
+alias syu="sudo pacman -Syu"
+alias asyu="yaourt -Sbu --aur"
+# system aliases
+alias sshn="sudo shutdown -h now"
+alias sr="sudo reboot"
