16299252ffb5c655199861b8444601bfd4e0baf5
Predrag Mijatovic <developej@gmail.com>
2013-12-05 19:24:01 +0100

    oh-my-zsh submodule update

--- symlink.sh
+++ symlink.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+cd /home/$USER/dotfiles/ &&
+git submodule init &&
+git submodule update &&
 ln -s /home/$USER/dotfiles/alias.zsh /home/$USER/dotfiles/.oh-my-zsh/custom/alias.zsh
 ln -s /home/$USER/dotfiles/.alsoftrc /home/$USER/.alsoftrc
 ln -s /home/$USER/dotfiles/.i3 /home/$USER/.i3
 ln -s /home/$USER/dotfiles/.i3/i3status.conf /home/$USER/.i3status.conf
 ln -s /home/$USER/dotfiles/.oh-my-zsh /home/$USER/.oh-my-zsh
+mkdir -p /home/$USER/.config/terminator
 ln -s /home/$USER/dotfiles/terminatorconfig /home/$USER/.config/terminator/config
 ln -s /home/$USER/dotfiles/.vimrc.local /home/$USER/.vimrc.local
 ln -s /home/$USER/dotfiles/.xinitrc /home/$USER/.xinitrc
